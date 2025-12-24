9a738b140806f7af40f8cf7ef00aa25d9ba0b6d8
imvishnudwivedi <vishnudehradun@gmail.com>
2025-12-24 11:58:04 +0530

    login and signup form

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,18 @@ const createToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: '3d'})
 }
 // login user
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
     // Logic for user login
     res.send({mesg: 'User login'});
 }
